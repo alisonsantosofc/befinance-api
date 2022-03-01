0865f0664e1acc1a65de7b593be53c5ced7ac250
alisonsantosofc <alisonsantoso22@gmail.com>
2022-03-01 15:02:43 -0300

    Create transactions custom repository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -3,15 +3,42 @@ import { EntityRepository, Repository } from 'typeorm';
 import Transaction from '../models/Transaction';
 
 interface Balance {
-  income: number;
-  outcome: number;
+  incoming: number;
+  outbound: number;
   total: number;
 }
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { incoming, outbound } = transactions.reduce(
+      (accumulator: Omit<Balance, 'total'>, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'incoming':
+            accumulator.incoming += Number(transaction.value);
+            break;
+
+          case 'outbound':
+            accumulator.outbound += Number(transaction.value);
+            break;
+
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        incoming: 0,
+        outbound: 0,
+      }
+    );
+
+    const total = incoming - outbound;
+
+    return { incoming, outbound, total };
   }
 }
 
