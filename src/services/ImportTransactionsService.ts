ea264b5d188d8fb5058607a698b77cf555a1eede
alisonsantosofc <alisonsantoso22@gmail.com>
2022-03-01 16:40:57 -0300

    Challenge finished

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,93 @@
+import fs from 'fs';
+import csvParse from 'csv-parse';
+import { getCustomRepository, getRepository, In } from 'typeorm';
+
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+import AppError from '../errors/AppError';
+
+interface TransactionCSV {
+  title: string;
+  type: 'incoming' | 'outbound';
+  value: number;
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filePath: string): Promise<Transaction[]> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    const contactReadStream = fs.createReadStream(filePath);
+
+    const parsers = csvParse({
+      from_line: 2,
+    });
+
+    const parseCSV = contactReadStream.pipe(parsers);
+
+    const transactions: TransactionCSV[] = [];
+    const categories: string[] = [];
+
+    parseCSV.on('data', async (line) => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim()
+      );
+
+      if (!title || !type || !value) {
+        throw new AppError(
+          'Required property does not exists in csv file line'
+        );
+      }
+
+      categories.push(category);
+
+      transactions.push({ title, type, value, category });
+    });
+
+    await new Promise((resolve) => parseCSV.on('end', resolve));
+
+    const existentCategories = await categoriesRepository.find({
+      where: {
+        title: In(categories),
+      },
+    });
+
+    const existentCategoriesTitle = existentCategories.map(
+      (category: Category) => category.title
+    );
+
+    const addCategoriesTitle = categories
+      .filter((category) => !existentCategoriesTitle.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    const newCategories = categoriesRepository.create(
+      addCategoriesTitle.map((title) => ({
+        title,
+      }))
+    );
+
+    await categoriesRepository.save(newCategories);
+
+    const allCategories = [...existentCategories, ...newCategories];
+
+    const newTransactions = transactionsRepository.create(
+      transactions.map((transaction) => ({
+        title: transaction.title,
+        type: transaction.type,
+        value: transaction.value,
+        category: allCategories.find(
+          (category) => category.title === transaction.category
+        ),
+      }))
+    );
+
+    await transactionsRepository.save(newTransactions);
+
+    await fs.promises.unlink(filePath);
+
+    return newTransactions;
   }
 }
 
